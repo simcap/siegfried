065a5f3e4e563d73fb9617fe92d924377fb7b272
simcap <simcap@fastmail.com>
2016-02-27 23:08:57 +0100

    Initial simple working ws server

---
+++ lib/siegfried.ex
@@ -0,0 +1,27 @@
+defmodule Siegfried do
+  @behaviour :application
+
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    children = [
+      worker(__MODULE__, [], function: :run)
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one, name: Siegfried.Supervisor)
+  end
+
+  def run do
+    routes = [
+      {"/ws", WsHandler, []}
+    ]
+
+    dispatch = :cowboy_router.compile([{:_, routes}])
+    {:ok, _} = :cowboy.start_http(:http, 100, [{:port, 8080}],
+                                  [{:env, [{:dispatch, dispatch}]}])
+  end
+
+  def stop(_state) do
+    :ok
+  end
+end
---
+++ test/siegfried_test.exs
@@ -0,0 +1,8 @@
+defmodule SiegfriedTest do
+  use ExUnit.Case
+  doctest Siegfried
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
